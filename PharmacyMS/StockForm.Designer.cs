1c25382698246817cdb4abc6ad34212b2039debd
chnaumangujjar0 <chnaumangujjar0@gmail.com>
2026-04-28 10:10:10 +0500

    Stock Management with history completed

--- PharmacyMS/StockForm.Designer.cs
+++ PharmacyMS/StockForm.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbMedicine = new System.Windows.Forms.ComboBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtRemarks = new System.Windows.Forms.TextBox();
+            this.btnAddStock = new System.Windows.Forms.Button();
+            this.lblLowStock = new System.Windows.Forms.Label();
+            this.dgvStock = new System.Windows.Forms.DataGridView();
+            this.dgvHistory = new System.Windows.Forms.DataGridView();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.pnlHeader.Controls.Add(this.label1);
+            this.pnlHeader.Location = new System.Drawing.Point(-1, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(934, 60);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.label1.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label1.Location = new System.Drawing.Point(300, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "📦 Stock Management";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(26, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Medicine :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(26, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Add Quantity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(26, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Remarks:";
+            // 
+            // cmbMedicine
+            // 
+            this.cmbMedicine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.cmbMedicine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMedicine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbMedicine.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMedicine.ForeColor = System.Drawing.Color.White;
+            this.cmbMedicine.FormattingEnabled = true;
+            this.cmbMedicine.Location = new System.Drawing.Point(30, 111);
+            this.cmbMedicine.Name = "cmbMedicine";
+            this.cmbMedicine.Size = new System.Drawing.Size(230, 27);
+            this.cmbMedicine.TabIndex = 4;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.txtQuantity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtQuantity.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtQuantity.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.ForeColor = System.Drawing.Color.White;
+            this.txtQuantity.Location = new System.Drawing.Point(30, 213);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(230, 27);
+            this.txtQuantity.TabIndex = 5;
+            // 
+            // txtRemarks
+            // 
+            this.txtRemarks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.txtRemarks.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRemarks.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtRemarks.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRemarks.ForeColor = System.Drawing.Color.White;
+            this.txtRemarks.Location = new System.Drawing.Point(30, 304);
+            this.txtRemarks.Name = "txtRemarks";
+            this.txtRemarks.Size = new System.Drawing.Size(230, 27);
+            this.txtRemarks.TabIndex = 6;
+            // 
+            // btnAddStock
+            // 
+            this.btnAddStock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnAddStock.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddStock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddStock.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddStock.ForeColor = System.Drawing.Color.White;
+            this.btnAddStock.Location = new System.Drawing.Point(30, 380);
+            this.btnAddStock.Name = "btnAddStock";
+            this.btnAddStock.Size = new System.Drawing.Size(230, 38);
+            this.btnAddStock.TabIndex = 7;
+            this.btnAddStock.Text = "📦 Add Stock";
+            this.btnAddStock.UseVisualStyleBackColor = false;
+            this.btnAddStock.Click += new System.EventHandler(this.btnAddStock_Click_1);
+            // 
+            // lblLowStock
+            // 
+            this.lblLowStock.AutoSize = true;
+            this.lblLowStock.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.lblLowStock.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblLowStock.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowStock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.lblLowStock.Location = new System.Drawing.Point(26, 466);
+            this.lblLowStock.Name = "lblLowStock";
+            this.lblLowStock.Padding = new System.Windows.Forms.Padding(10);
+            this.lblLowStock.Size = new System.Drawing.Size(237, 41);
+            this.lblLowStock.TabIndex = 8;
+            this.lblLowStock.Text = "Low Stock Medicines: 0";
+            // 
+            // dgvStock
+            // 
+            this.dgvStock.AllowUserToAddRows = false;
+            this.dgvStock.AllowUserToDeleteRows = false;
+            this.dgvStock.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvStock.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.dgvStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStock.GridColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dgvStock.Location = new System.Drawing.Point(337, 79);
+            this.dgvStock.Name = "dgvStock";
+            this.dgvStock.ReadOnly = true;
+            this.dgvStock.RowHeadersVisible = false;
+            this.dgvStock.RowHeadersWidth = 51;
+            this.dgvStock.RowTemplate.Height = 24;
+            this.dgvStock.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvStock.Size = new System.Drawing.Size(583, 220);
+            this.dgvStock.TabIndex = 9;
+            // 
+            // dgvHistory
+            // 
+            this.dgvHistory.AllowUserToAddRows = false;
+            this.dgvHistory.AllowUserToDeleteRows = false;
+            this.dgvHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHistory.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(31)))), ((int)(((byte)(56)))));
+            this.dgvHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistory.Location = new System.Drawing.Point(337, 327);
+            this.dgvHistory.Name = "dgvHistory";
+            this.dgvHistory.ReadOnly = true;
+            this.dgvHistory.RowHeadersVisible = false;
+            this.dgvHistory.RowHeadersWidth = 51;
+            this.dgvHistory.RowTemplate.Height = 24;
+            this.dgvHistory.Size = new System.Drawing.Size(583, 180);
+            this.dgvHistory.TabIndex = 10;
+            // 
+            // StockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "StockForm";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(46)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(932, 553);
+            this.Controls.Add(this.dgvHistory);
+            this.Controls.Add(this.dgvStock);
+            this.Controls.Add(this.lblLowStock);
+            this.Controls.Add(this.btnAddStock);
+            this.Controls.Add(this.txtRemarks);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.cmbMedicine);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pnlHeader);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.Name = "StockForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stock Management";
+            this.Load += new System.EventHandler(this.StockForm_Load);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbMedicine;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtRemarks;
+        private System.Windows.Forms.Button btnAddStock;
+        private System.Windows.Forms.Label lblLowStock;
+        private System.Windows.Forms.DataGridView dgvStock;
+        private System.Windows.Forms.DataGridView dgvHistory;
     }
 }
--- PharmacyMS/StockForm.cs
+++ PharmacyMS/StockForm.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace PharmacyMS
 {
@@ -16,5 +11,212 @@ namespace PharmacyMS
         {
             InitializeComponent();
         }
+
+        // ── Form Load ─────────────────────────────────────────
+        private void StockForm_Load(object sender, EventArgs e)
+        {
+            LoadMedicinesDropdown(); // ← Fill combobox
+            LoadStockGrid();         // ← Load stock table
+            LoadStockHistory();      // ← Load history table
+        }
+
+        // ── Load Medicines into Dropdown ──────────────────────
+        private void LoadMedicinesDropdown()
+        {
+            try
+            {
+                string query = "SELECT MedicineId, Name, Stock FROM Medicines ORDER BY Name ASC";
+
+                using (SqlConnection con = DBHelper.GetConnection())
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    cmbMedicine.DataSource = table;
+                    cmbMedicine.DisplayMember = "Name";       // ← Shows medicine name
+                    cmbMedicine.ValueMember = "MedicineId"; // ← Stores ID
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("❌ Error loading medicines:\n" + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ── Load Stock Grid ───────────────────────────────────
+        private void LoadStockGrid()
+        {
+            try
+            {
+                string query = @"SELECT 
+                                    Name        AS 'Medicine',
+                                    Category    AS 'Category',
+                                    Stock       AS 'Current Stock',
+                                    ExpiryDate  AS 'Expiry Date',
+                                    Supplier    AS 'Supplier',
+                                    CASE 
+                                        WHEN Stock < 10 THEN ' Low'
+                                        WHEN Stock < 30 THEN ' Medium'
+                                        ELSE ' Good'
+                                    END AS 'Status'
+                                FROM Medicines
+                                ORDER BY Stock ASC";
+
+                using (SqlConnection con = DBHelper.GetConnection())
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvStock.DataSource = table;
+                }
+
+                // ← Update low stock count label
+                UpdateLowStockLabel();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("❌ Error loading stock:\n" + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ── Load Stock History ────────────────────────────────
+        private void LoadStockHistory()
+        {
+            try
+            {
+                string query = @"SELECT 
+                                    m.Name       AS 'Medicine',
+                                    h.AddedStock AS 'Stock Added',
+                                    h.AddedBy    AS 'Added By',
+                                    h.Remarks    AS 'Remarks',
+                                    h.AddedDate  AS 'Date & Time'
+                                FROM StockHistory h
+                                JOIN Medicines m ON h.MedicineId = m.MedicineId
+                                ORDER BY h.AddedDate DESC";
+
+                using (SqlConnection con = DBHelper.GetConnection())
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvHistory.DataSource = table;
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("❌ Error loading history:\n" + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // ── Update Low Stock Label ────────────────────────────
+        private void UpdateLowStockLabel()
+        {
+            try
+            {
+                string query = "SELECT COUNT(*) FROM Medicines WHERE Stock < 10";
+
+                using (SqlConnection con = DBHelper.GetConnection())
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    int count = (int)cmd.ExecuteScalar();
+                    lblLowStock.Text = "⚠️ Low Stock Medicines: " + count;
+                    lblLowStock.ForeColor = count > 0
+                        ? System.Drawing.Color.FromArgb(231, 76, 60)   // ← Red
+                        : System.Drawing.Color.FromArgb(79, 195, 161); // ← Teal
+                }
+            }
+            catch { }
+        }
+
+        // ── Add Stock Button ──────────────────────────────────
+        private void btnAddStock_Click_1(object sender, EventArgs e)
+        {
+            // ✅ Validation
+            if (cmbMedicine.SelectedValue == null)
+            {
+                MessageBox.Show("Please select a Medicine!",
+                                "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtQuantity.Text.Trim()))
+            {
+                MessageBox.Show("Please enter Quantity!",
+                                "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtQuantity.Focus();
+                return;
+            }
+
+            if (!int.TryParse(txtQuantity.Text, out int qty) || qty <= 0)
+            {
+                MessageBox.Show("Please enter a valid Quantity (must be greater than 0)!",
+                                "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtQuantity.Focus();
+                return;
+            }
+
+            int medicineId = (int)cmbMedicine.SelectedValue;
+            string medicineName = cmbMedicine.Text;
+            string remarks = string.IsNullOrEmpty(txtRemarks.Text.Trim())
+                             ? "Stock added" : txtRemarks.Text.Trim();
+
+            try
+            {
+                using (SqlConnection con = DBHelper.GetConnection())
+                {
+                    con.Open();
+
+                    // ✅ Update Medicine Stock
+                    string updateQuery = "UPDATE Medicines SET Stock = Stock + @qty WHERE MedicineId = @id";
+                    using (SqlCommand cmd = new SqlCommand(updateQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@qty", qty);
+                        cmd.Parameters.AddWithValue("@id", medicineId);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // ✅ Insert into Stock History
+                    string historyQuery = @"INSERT INTO StockHistory 
+                                           (MedicineId, AddedStock, Remarks, AddedBy)
+                                           VALUES (@id, @qty, @remarks, @by)";
+                    using (SqlCommand cmd = new SqlCommand(historyQuery, con))
+                    {
+                        cmd.Parameters.AddWithValue("@id", medicineId);
+                        cmd.Parameters.AddWithValue("@qty", qty);
+                        cmd.Parameters.AddWithValue("@remarks", remarks);
+                        cmd.Parameters.AddWithValue("@by", "Admin");
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show($"✅ {qty} units added to {medicineName} successfully!",
+                                "Stock Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // ✅ Clear & Refresh
+                txtQuantity.Clear();
+                txtRemarks.Clear();
+                LoadMedicinesDropdown();
+                LoadStockGrid();
+                LoadStockHistory();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("❌ Error updating stock:\n" + ex.Message,
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
     }
 }
